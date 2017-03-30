1b5cecf1c22acdf37956500f69bb02dd07e19fe0
zaxh <1443676226@qq.com>
2017-03-30 17:10:05 +0800

    导航修改，案例列表展示修改

--- js/container.js
+++ js/container.js
@@ -3,4 +3,8 @@
  */
 $(function(){
     $(".container").height($(window).height()-$("header").height()-$("footer").height());
+    $(".container .tab-wrap .tab").click(function(){
+        $(this).addClass("active").siblings().removeClass("active");
+        $(".container .con ul").eq($(this).index()).removeClass("hidden").siblings().addClass("hidden");
+    });
 });
